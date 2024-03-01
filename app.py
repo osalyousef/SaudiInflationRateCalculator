9270892f671674f1f41a79068e9c46e44bba1685
osalyousef <os.forwork1@gmail.com>
2024-03-01 05:17:32 +0300

    AlmostFinishedAboutMeLeft

--- app.py
+++ app.py
@@ -25,5 +25,9 @@ def calculate():
     
     return render_template('index.html', result=future_value)
 
+
+@app.route ("/about")
+def about():
+    return render_template('about.html')
 if __name__ == "__main__":
     app.run(debug=True)
