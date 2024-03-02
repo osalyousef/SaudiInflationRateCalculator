3c34b57f2c55af6a4d6a6c0da4a6bb3ca8ca2739
osalyousef <os.forwork1@gmail.com>
2024-03-02 19:31:24 +0300

    ProjectFinishedYIPIIIIIEEEE

--- app.py
+++ app.py
@@ -22,12 +22,8 @@ def calculate():
         future_value = "Error: Inflation rate cannot be zero."
     except ValueError:
         future_value = "Error: Invalid input. Please enter a valid number."
-    
+
     return render_template('index.html', result=future_value)
 
-
-@app.route ("/about")
-def about():
-    return render_template('about.html')
 if __name__ == "__main__":
     app.run(debug=True)
